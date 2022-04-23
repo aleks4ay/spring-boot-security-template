c560228db6b2e10851739da2369028e73f1cb6ae
aleks4ay <aleks4ay@gmail.com>
2022-04-23 18:01:30 +0300

    In memory UserDetailsService (with 'user' and 'admin').

--- src/main/java/org/aleks4ay/springbootsecurity/SecurityConfig.java
+++ src/main/java/org/aleks4ay/springbootsecurity/SecurityConfig.java
@@ -1,8 +1,13 @@
 package org.aleks4ay.springbootsecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,5 +20,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
             .and()
                 .logout().logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$12$1hBPbesIeA3/tM7wNn9fR.mxReWgH2p6STEHYuSuGBbzPIQRpi0gK") //100
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$12$E7ypqHeJWBfABv/WsJRFWecgzQKeYkUyFaS/x4G9Mq6ooq8OK7Wtm") //admin
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
